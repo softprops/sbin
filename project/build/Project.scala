620776a5b9b81a9a3388a5844ddc53d86b96d179
softprops <d.tangren@gmail.com>
2010-07-30 13:59:58 -0400

    2.8 final-ize

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,11 +3,11 @@ import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   // persistence
   val akkaRepo = "akka repo" at "http://www.scalablesolutions.se/akka/repository"
-  val redis = "com.redis" % "redisclient" % "2.8.0.RC3-1.4"
+  val redis = "com.redis" % "redisclient" % "2.8.0-1.4"
   
   // web
   val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.RC0" % "test"
   
   // unfiltered
-  val ufs = "net.databinder" %% "unfiltered-server" % "0.1.3-SNAPSHOT"
+  val ufs = "net.databinder" %% "unfiltered-server" % "0.1.3"
 }
